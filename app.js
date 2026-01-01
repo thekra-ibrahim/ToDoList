cbca0b9e501086a582cf0fa380c3b4d66d6279cc
thekra-ibrahim <168662675+thekra-ibrahim@users.noreply.github.com>
2026-01-01 20:35:46 +0200

    Add modal confirmation logic and initialize app

--- app.js
+++ app.js
@@ -75,5 +75,25 @@ const input = document.getElementById("modalInput");
 function closeModal() {
   document.getElementById("modal").classList.add("hidden");
 }
+function modalConfirm() {
+  const input = document.getElementById("modalInput");
+  const msg = document.getElementById("modalMsg");
+
+  if (modalConfig.needsInput) {
+    const val = input.value.trim();
+    if (!isValidTask(val)) {
+      msg.textContent = "Name must be ≥ 5 characters and not start with a number.";
+      return;
+    }
+    modalConfig.confirm(val);
+  } else {
+    modalConfig.confirm();
+  }
+
+  closeModal();
+}
+
+loadTodos();
+renderTasks();
 
 
