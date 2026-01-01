b1ed1177d6604e077343216d1dfd64a0ecf336e3
Malak-Abualrob <153291071+Malak-Abualrob@users.noreply.github.com>
2026-01-01 19:58:59 +0200

    Load tasks from local storage
    Add loadTodos function to initialize tasks from localStorage

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 let tasks = [];
 let filter = 'all';
+
+function loadTodos() {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    tasks = JSON.parse(storedTasks);
+  } else {
+    tasks = [];
+  }
+}
