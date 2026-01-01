66c0563357019d07e755c3b0c6b2b1393ff793a2
thekra-ibrahim <168662675+thekra-ibrahim@users.noreply.github.com>
2026-01-01 20:27:54 +0200

    Handle single task deletion

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = "🗑️";
     deleteBtn.className = "delete";
+deleteBtn.onclick = () => {
+      openModal({
+        title: "Delete Task?",
+        msg: "Are you sure you want to delete this task?",
+        confirm: () => {
+          const taskIndex = tasks.findIndex(t => t.id === task.id);
+          if (taskIndex > -1) {
+            tasks.splice(taskIndex, 1);
+            saveTodos();
+            renderTasks();
+          }
+        },
+        needsInput: false
+      });
+    };
+
